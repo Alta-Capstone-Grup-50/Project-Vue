51da3c9d5e2eec6f5247bb6f914ee9ee29151629
syafikadin <syafik.adin222@gmail.com>
2022-06-15 14:57:50 +0700

    Test Page

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,13 +1,45 @@
 <template>
-        <NavbarPage/>
+<b-container>
+    <div class="d-flex justify-content-start">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+    <div class="d-flex justify-content-end">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+    <div class="d-flex justify-content-center">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+    <div class="d-flex justify-content-between">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+    <div class="d-flex justify-content-around">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+    <div class="d-flex justify-content-evenly">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+
+    <div class="row justify-content-between">
+        <div class="col-4">
+            One of two columns
+        </div>
+        <div class="col-4">
+            One of two columns
+        </div>
+    </div>
+</b-container>
 </template>
 
 <script>
-import NavbarPage from '@/components/NavbarPage.vue'
 export default {
     name: 'TestPageView',
     components:{
-        NavbarPage
     }
 }
 </script>
