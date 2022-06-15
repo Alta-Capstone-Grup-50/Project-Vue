b6524ce9fee9761b24895a16f620a6d1d2341de1
Chandralaksana22 <chandralaksana225@gmail.com>
2022-06-15 15:31:53 +0700

    Fix Navbar

---
+++ src/layout/default.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <Navbar />
+      <router-view/>
+</div>
+</template>
+<script>
+import Navbar from "@/components/navbar.vue"
+export default {
+  name: 'DefaultPage',
+  components: {
+      Navbar
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,57 @@
 import Vue from "vue";
 import VuerRouter from "vue-router";
-import LoginPage from "@/views/LoginPage";
+import HomeAdmin from "@/views/admin/HomeAdmin";
 import DataDokter from "@/views/admin/DataDokter";
 import HomeDokter from "@/views/dokter/HomeDokter";
+import Default from "@/layout/default.vue";
+import LoginPage from "@/views/LoginPage";
+
+
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.use(VuerRouter);
 
 const routes = [
   {
     path: "/",
-    name: "LoginPage",
-    component : LoginPage,
-
+    name: "Layout",
+    component :Default,
+    children : [
+      {
+        path:"/HomeAdmin",
+        name:"HomeAdmin",
+        component: HomeAdmin,
+      },
+      {
+        path:"/",
+        name:"DataDokter",
+        component: DataDokter,
+      },
+      {
+        path:"/HomeDokter",
+        name:"HomeDokter",
+        component: HomeDokter,
+      }
+    ]
+    
   },
   {
-    path: "/HomeDokter",
-    name: "HomeDokter",
-    component: HomeDokter,
-  },
-  {
-    path: "/DataDokter",
-    name: "DataDokter",
-    component : DataDokter,
+    path:"/LoginPage",
+    name:"LoginPage",
+    component: LoginPage,
   },
 ];
 const router = new VuerRouter({
     mode:"history",
-    base: process.env.BASE_URL,
+    base: "/",
     routes,
-  })
+  });
 
 export default router;
