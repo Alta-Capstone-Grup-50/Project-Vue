d69ee9371c446dd6ee1d8bc9cc185d8a3a715fcd
Chandralaksana22 <chandralaksana225@gmail.com>
2022-06-24 02:55:05 +0700

    Update HomeAdmin

--- src/views/admin/HomeAdmin.vue
+++ src/views/admin/HomeAdmin.vue
@@ -4,66 +4,37 @@
       <div>
         <b-img src="https://i.postimg.cc/wBYm6Dvv/beautiful-shot-ponte-vecchio-florence-tuscany-italy-1.png" rounded class="mt-5 shadow" fluid alt="Responsive image"></b-img>
       </div>
-      
-      <b-row class="d-flex justify-content-center mt-5">
-        <b-col cols="12" md="4">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
+      <div class="container mt-5 mb-5">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="https://i.postimg.cc/rpXF7vBX/Frame-12-1.png" class="card-img-top" alt="">
+              <div class="card-body text-center">
+                <p class="card-text text-center"><router-link class="text-decoration-none text-black" to="/DataPasien"><strong>Data Pasien</strong></router-link></p>
+                <a href="#" class="btn btn-primary"><router-link class="text-decoration-none text-white" to="/DataPasien"><strong>Lebih Lanjut</strong></router-link></a>
+              </div>
+            </div>
           </div>
-        </b-col>
-        <b-col cols="12" md="4">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="https://i.postimg.cc/rpXF7vBX/Frame-12-1.png" class="card-img-top" alt="">
+              <div class="card-body text-center">
+                <p class="card-text text-center"><router-link class="text-decoration-none text-black" to="/DataDokter"><strong>Data Dokter</strong></router-link></p>
+                <a href="#" class="btn btn-primary"><router-link class="text-decoration-none text-white" to="/DataDokter"><strong>Lebih Lanjut</strong></router-link></a>
+              </div>
+            </div>
           </div>
-        </b-col>
-        <b-col cols="12" md="4">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="https://i.postimg.cc/rpXF7vBX/Frame-12-1.png" class="card-img-top" alt="">
+              <div class="card-body text-center">
+                <p class="card-text text-center"><router-link class="text-decoration-none text-black" to="/DataPerawat"><strong>Data Perawat</strong></router-link></p>
+                <a href="#" class="btn btn-primary"><router-link class="text-decoration-none text-white" to="/DataPerawat"><strong>Lebih Lanjut</strong></router-link></a>
+              </div>
+            </div>
           </div>
-        </b-col>
-      </b-row>
+        </div>
+      </div>
     </b-container>
   </div>
 </template>
